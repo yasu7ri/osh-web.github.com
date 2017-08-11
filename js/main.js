f6afd2f061357c1cc0d097277cc01d376988aae1
Tomohiko Himura <eiel.hal@gmail.com>
2017-08-11 12:06:32 +0900

    fix https

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
-require(["http://code.jquery.com/jquery-1.7.2.min.js"], function($) {
-  require(["http://code.angularjs.org/angular-1.0.1.min.js"], function () {
+require(["https://code.jquery.com/jquery-1.7.2.min.js"], function($) {
+  require(["https://code.angularjs.org/angular-1.0.1.min.js"], function () {
     require(["project"]);
   });
   require(["js/bootstrap.min.js"]);
